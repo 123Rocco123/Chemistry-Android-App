dc087141f2944db7564eab35c9af40eaa7348b06
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-20 00:37:25 +0100

    Submit Button Action Listener

--- ChemistryAndroidApp/app/src/main/java/com/example/androidchemistryapp/MainActivity.java
+++ ChemistryAndroidApp/app/src/main/java/com/example/androidchemistryapp/MainActivity.java
@@ -4,11 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Contains the name of the Element
+    String name;
+
+    // Contains the input field
+    EditText nameInput;
+
+    // Contains the button
+    Button submitButton;
+
+    private void popUpMessage(String elementToSearch) {
+        // The "Toast.LENGTH_SHORT" is used to determine the duration that the pop-up message stays on screen for.
+        Toast.makeText(MainActivity.this, ("Searching Element: " + elementToSearch), Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        // Similar to the "addActionListener" in Swing
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = (nameInput.getText()).toString();
+
+                popUpMessage(name);
+            }
+        });
     }
 }
